4c684c9369d90a8f9c0017406409fa2a39aa68a0
Joren LeGoullon <lego5858joren@gmail.com>
2024-09-02 18:41:35 -0400

    Initial commit of ACME-System project changed the helloworld print

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello from IntelliJ");
+        System.out.println("Hello from Homework 11");
     }
 }
