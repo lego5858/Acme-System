c879d13465beff5ec9cd12d8ef4d451236ecf57b
Joren LeGoullon <lego5858joren@gmail.com>
2024-09-02 19:25:08 -0400

    changed the print line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Person 2");
+        System.out.println("Hello ,here are the new changes");
     }
 }
